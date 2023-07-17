03564e44644080cf52fae6a37a36b5d40132af6e
guarnerman <gs.ulises18@gmail.com>
2023-07-17 14:33:10 -0600

    añadí objeto de círculo y retorno de objeto con calculo de perimetro y area.

--- math.js
+++ math.js
@@ -1,3 +1,5 @@
+console.group('Cuadrado');
+
 const ladoCuadrado = 5;
 const perimetroCuadrado = ladoCuadrado * 4;
 const areaCuadrado = ladoCuadrado * ladoCuadrado;
@@ -7,7 +9,9 @@ console.log({
   perimetroCuadrado,
   areaCuadrado,
 });
+console.groupEnd('Cuadrado');
 
+console.group('Triangulo');
 const ladoTriangulo1 = 6;
 const ladoTriangulo2 = 6;
 const ladoTriangulo3 = 4;
@@ -15,6 +19,7 @@ const ladoTriangulo3 = 4;
 const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + ladoTriangulo3;
 const S = perimetroTriangulo / 2;
 const areaTriangulo = Math.sqrt(S * (S - ladoTriangulo1) * (S - ladoTriangulo2) * (S - ladoTriangulo3));
+
 
 function calcularCuadrado(lado) {
   return {
@@ -37,4 +42,33 @@ console.log({
   perimetroTriangulo,
   areaTriangulo,
 });
+console.groupEnd('Triangulo');
 
+console.group('Circle')
+
+const radioCirculo = 3;
+const diametroCirculo = radioCirculo * 2;
+const PI = Math.PI;
+
+const circunferencia = diametroCirculo * PI;
+const AreaCirculo = (radioCirculo ** 2) * PI;
+
+console.log({
+  radioCirculo,
+  diametroCirculo,
+  PI,
+  circunferencia,
+  AreaCirculo,
+});
+
+function calcularCirculo(radio) {
+  const diametro = radio * 2;
+  const radioCuadrado = Math.pow(radio, 2);
+
+  return {
+    circunferencia: diametro * Math.PI,
+    area: radioCuadrado * Math.PI,
+  }
+}
+
+console.groupEnd('Circle')
