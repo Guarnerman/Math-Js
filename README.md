9df320b25e7a832898d3082070a0bb6ab8734e7f
guarnerman <gs.ulises18@gmail.com>
2023-07-17 19:11:07 -0600

    metí un objeto para controlar una cuponera y te haga el descuento

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Math-Js
+
+- Desarrollo de fórmulas en JavaScript para aplicarlo alas funcionalidades que necesite usar después, po rel momento he ocupado un par de métodos de Math y calculado perímetro y área de un cuadrado, triángulo y círculo.
+
+- Lo puedo manejar como constantes individuales o también como objetos
---
+++ descuentos.js
@@ -0,0 +1,44 @@
+const inputPrice = document.querySelector('#price');
+const inputCupon = document.querySelector('#cupon');
+const btn = document.querySelector('#calcular');
+const result = document.querySelector('#result');
+
+const cuponario = {
+  'SUMMERTIME': 35,
+  'BLACKFR1DAY': 40,
+  'BUENFIN': 30,
+  'NewYear': 15,
+}
+
+btn.addEventListener('click', calcularPrecioDescuento);
+
+function calcularPrecioDescuento() {
+  const price = Number(inputPrice.value);
+  const cupon = inputCupon.value;
+  let discount = 50
+  
+  const total = (price * (1 - (discount / 100)));
+
+  if (!price || !discount) {
+    result.innerText = 'Dejaste una casilla en blanco';
+  }
+
+  for (const cup in cuponario) {
+    console.log(typeof(cup), cup, typeof(cupon), cupon);
+    if (cup === cupon) {
+      result.innerText = 'Descuento aplicado, el precio es $' + total;
+      return
+    } else {
+      result.innerText = 'Cupón no válido';
+    }
+    
+  }
+  
+  if (discount >= 100 || discount < 0) {
+    result.innerText = 'El descuento debe estar en el rango %';
+    return
+  }
+  
+  /* result.innerText = 'El nuevo precio con descuento es: $' + total; */
+
+}
