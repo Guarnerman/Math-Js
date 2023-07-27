ee194a4a4a51c1d285c3afeb8d5fb20b00a6c1e1
Guarnerman <126298667+Guarnerman@users.noreply.github.com>
2023-07-26 23:08:54 -0600

    se aplica el descuento usanto un condicional que devuelve true cuando el input existe en el objeto cuponario y aplica el descuento

--- descuentos.js
+++ descuentos.js
@@ -1,44 +1,33 @@
-const inputPrice = document.querySelector('#price');
-const inputCupon = document.querySelector('#cupon');
-const btn = document.querySelector('#calcular');
-const result = document.querySelector('#result');
+const inputPrice = document.querySelector("#price");
+const inputCupon = document.querySelector("#cupon");
+const btn = document.querySelector("#calcular");
+const result = document.querySelector("#result");
 
 const cuponario = {
-  'SUMMERTIME': 35,
-  'BLACKFR1DAY': 40,
-  'BUENFIN': 30,
-  'NewYear': 15,
-}
+  SUMMERTIME: 35,
+  BLACKFR1DAY: 40,
+  BUENFIN: 30,
+  NewYear: 15,
+};
 
-btn.addEventListener('click', calcularPrecioDescuento);
+btn.addEventListener("click", calcularPrecioDescuento);
 
 function calcularPrecioDescuento() {
   const price = Number(inputPrice.value);
   const cupon = inputCupon.value;
-  let discount = 50
-  
-  const total = (price * (1 - (discount / 100)));
+  let discount;
 
   if (!price || !discount) {
-    result.innerText = 'Dejaste una casilla en blanco';
+    result.innerText = "Dejaste una casilla en blanco";
   }
 
-  for (const cup in cuponario) {
-    console.log(typeof(cup), cup, typeof(cupon), cupon);
-    if (cup === cupon) {
-      result.innerText = 'Descuento aplicado, el precio es $' + total;
-      return
-    } else {
-      result.innerText = 'Cupón no válido';
-    }
-    
+  if (cuponario[cupon]) {
+    discount = cuponario[cupon];
+    const total = price * (1 - discount / 100);
+    console.log(cuponario[cupon]);
+    result.innerText = "Descuento aplicado, el precio es $" + total;
+    return;
+  } else {
+    result.innerText = "Cupón no válido";
   }
-  
-  if (discount >= 100 || discount < 0) {
-    result.innerText = 'El descuento debe estar en el rango %';
-    return
-  }
-  
-  /* result.innerText = 'El nuevo precio con descuento es: $' + total; */
-
 }
