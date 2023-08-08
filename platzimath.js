c4bec48a9efc2e5e078778435e8e4e9926628a97
guarnerman <gs.ulises18@gmail.com>
2023-08-08 17:33:47 -0600

    calcular media y mediana

--- platzimath.js
+++ platzimath.js
@@ -6,7 +6,20 @@ function esImpar(lista) {
 }
 
 function calcularMediana(lista) {
-  
+  const listaEsPar = esPar(lista);
+
+  if (listaEsPar) {
+    const indexMitad1ListaPar = (lista.length / 2) -1
+    const indexMitad2ListaPar = (lista.length / 2);
+
+    calcularPromedio([lista[indexMitad1ListaPar], lista[indexMitad2ListaPar]])
+  } else {
+    const indexMitadListaImpar = Math.floor(lista.length / 2);
+    const medianaListaImpar = lista[indexMitadListaImpar];
+    console.log(indexMitadListaImpar);
+    console.log(medianaListaImpar);
+    return medianaListaImpar;
+  }
 }
 
 function calcularPromedio(lista) {
